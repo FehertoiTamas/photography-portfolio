ce3aa0548ff448a7df9dc621f13edc8f85ee71f0
Tamas <tamas.fehertoi@gmail.com>
2025-01-04 15:12:30 -0500

    navbar

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,50 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react'
 import './Hero.css'
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Image from 'next/image';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+
+  useEffect(() => {
+    gsap.fromTo(
+      ".hero-title",
+      { opacity: 0, scale: 0 }, // Kezdőállapot
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 2,
+        ease: "sine.inOut",
+        scrollTrigger: {
+          trigger: ".navbar-text",
+          start: "center 20%",
+          end: "bottom 10%",
+          scrub: true,
+          markers: false,
+        },
+      }
+    );
+
+
+    // Cleanup: Törlés a komponens eltávolításakor
+    return () => {
+      animation.kill(); // Az animáció törlése
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill()); // Minden ScrollTrigger törlése
+    };
+
+  }, [])
+
   return (
     <>
       <section className='hero'>
-        <h1>Hero</h1>
+        <Image src="/hero.webp" alt="Hero Image"
+          layout="fill"     // Makes the image fill the container
+          objectFit="cover" // Ensures the image covers the section
+          priority />
+        <h1 className='hero-title'>Portfolio by Eva Sipos</h1>
       </section>
     </>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,79 +2,137 @@
 import { useEffect } from 'react'
 import './Navbar.css'
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Navbar() {
   useEffect(() => {
+    gsap.fromTo(
+      ".navbar-text",
+      { opacity: 1, scale: 1, transformOrigin: "left bottom" }, // Kezdőállapot
+      {
+        opacity: 0,
+        scale: 0.4,
+        duration: 2,
+        ease: "sine.inOut",
+        scrollTrigger: {
+          trigger: ".navbar-text",
+          start: "center 20%",
+          end: "bottom 10%",
+          scrub: true,
+          markers: false,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".logo",
+      { opacity: 0, scale: 0, transformOrigin: "right bottom" }, // Kezdőállapot
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 2,
+        ease: "sine.inOut",
+        scrollTrigger: {
+          trigger: ".navbar-text",
+          start: "center 20%",
+          end: "bottom 10%",
+          scrub: true,
+          markers: false,
+        },
+      }
+    );
+
+    // Linkek mozgása jobbra
+    gsap.fromTo(
+      ".links",
+      { opacity: 0, scale: 0 }, // Kezdőállapot
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 2,
+        ease: "sine.inOut",
+        scrollTrigger: {
+          trigger: ".navbar-text",
+          start: "center 20%",
+          end: "bottom 10%",
+          scrub: true,
+          markers: false,
+        },
+      }
+    );
+
+    // Cleanup: Törlés a komponens eltávolításakor
+    return () => {
+      animation.kill(); // Az animáció törlése
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill()); // Minden ScrollTrigger törlése
+    };
 
   }, [])
 
   return (
     <>
-      <nav className='navbar'>
-        <div className='logo'>Photography</div>
-        <ul>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="#"
-              data-text="Home"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="#"
-              data-text="Blogs"
-            >
-              Blogs
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="#"
-              data-text="Coding"
-            >
-              Coding
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="#"
-              data-text="Skills"
-            >
-              Skills
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="#"
-              data-text="Extras"
-            >
-              Extras
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="#"
-              data-text="Contact"
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <p className='navbar-text'>Photography</p>
+      <section className='navbar-container'>
+        <nav className='navbar'>
+          <div className='logo'>Photography</div>
+          <ul className='links'
+          >
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+                data-text="Home"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+                data-text="Blogs"
+              >
+                About me
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+                data-text="Coding"
+              >
+                My works
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+                data-text="Extras"
+              >
+                Extras
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+                data-text="Contact"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <p className='navbar-text'>Photography</p>
+      </section>
     </>
   )
 }
