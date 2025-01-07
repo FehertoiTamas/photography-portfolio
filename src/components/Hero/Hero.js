18fc494247e3109694355235f32a9f48d917e02a
Tamas <tamas.fehertoi@gmail.com>
2025-01-07 17:14:26 -0500

    add social icons and animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,11 @@ export default function Hero() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const animation = gsap.fromTo(
+
+      // Animációk és trigger-ek gyűjtése
+      const animations = [];
+
+      const animation1 = gsap.fromTo(
         ".hero-title",
         { opacity: 0, scale: 0 }, // Kezdőállapot
         {
@@ -31,10 +35,52 @@ export default function Hero() {
         }
       );
 
-      // Cleanup: Törlés a komponens eltávolításakor
+      animations.push(animation1);
+
+      const animation2 = gsap.fromTo(
+        ".social-icon3",
+        { x: "300%" },
+        {
+          x: "0%",
+          scrollTrigger: {
+            trigger: ".social-icon3",
+            start: "top bottom",
+            end: "+=500",
+            scrub: 1.5,
+            toggleActions: "play none none reverse",
+          },
+          ease: "power2.out",
+        }
+      );
+
+      animations.push(animation2);
+
+      const animation3 = gsap.fromTo(
+        ".social-icon1",
+        { x: "-300%" },
+        {
+          x: "0%",
+          scrollTrigger: {
+            trigger: ".social-icon1",
+            start: "top bottom",
+            end: "+=500",
+            scrub: 1.5,
+            toggleActions: "play none none reverse",
+          },
+          ease: "power2.out",
+        }
+      );
+
+      animations.push(animation3);
+
+
+
+      // Cleanup funkció a komponens eltávolításakor
       return () => {
-        animation.kill(); // Az animáció törlése
-        ScrollTrigger.getAll().forEach((trigger) => trigger.kill()); // Minden ScrollTrigger törlése
+        // Minden animáció törlése
+        animations.forEach((animation) => animation.kill());
+        // Minden ScrollTrigger törlése
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
       };
     }
   }, []);
@@ -52,13 +98,13 @@ export default function Hero() {
 
         {/* Social Icons */}
         <div className="social-icons-wrapper">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon social-icon1">
             <SiFacebook size={52} />
           </a>
           <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
             <SiInstagram size={52} />
           </a>
-          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer" className="social-icon social-icon3">
             <SiTiktok size={52} />
           </a>
         </div>
