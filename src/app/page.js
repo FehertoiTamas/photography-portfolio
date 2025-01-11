9ce4f3b45bf98b7b572ea24cf5802086d148af03
Tamas <tamas.fehertoi@gmail.com>
2025-01-10 19:17:17 -0500

    add contact page

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,6 @@ import Hero from "@/components/Hero/Hero";
 import AboutMe from "@/components/AboutMe/AboutMe";
 import Portfolio from "@/components/Portfolio/Portfolio";
 
-
-
 export default function Home() {
 
   return (
