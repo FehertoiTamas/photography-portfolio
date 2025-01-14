7e38f33fd5e1303dfa6c6ddba68198ac7ae9487e
Tamas <tamas.fehertoi@gmail.com>
2025-01-14 12:44:44 -0500

    contact me page fix hydration problems

--- src/app/contactMe/page.js
+++ src/app/contactMe/page.js
@@ -1,6 +1,6 @@
 "use client";
 import "./contactMe.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import PagesNav from "@/components/PagesNav/PagesNav";
 import { SiFacebook, SiTiktok, SiInstagram, SiWhatsapp } from "react-icons/si";
 import { FiMail } from "react-icons/fi";
@@ -9,30 +9,16 @@ import { useTranslation } from "react-i18next";
 const ContactMe = () => {
   const { t } = useTranslation();
   const [isMounted, setIsMounted] = useState(false);
-  const [videoEnded, setVideoEnded] = useState(false);
-  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isVideoFinished, setIsVideoFinished] = useState(false);
 
-  // Kezdeti mount kezelése
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-  const handleVideoEnd = () => {
-    setVideoEnded(true);
-  };
+  const handleVideoEnd = useCallback(() => {
+    setIsVideoFinished(true);
+  }, []);
 
-  // Form megjelenítése csak client oldalon
-  useEffect(() => {
-    if (!isMounted) return;
-
-    if (videoEnded) {
-      setTimeout(() => {
-        setIsFormVisible(true);
-      }, 800);
-    }
-  }, [videoEnded, isMounted]);
-
-  // SSR és kezdeti kliens oldali render
   if (!isMounted) {
     return (
       <section className="background-video-container">
@@ -48,24 +34,23 @@ const ContactMe = () => {
     );
   }
 
-  // Teljes render animációkkal és interaktivitással
   return (
     <section className="background-video-container">
       <PagesNav />
       <video
         autoPlay
         muted
-        className={`background-video ${videoEnded ? "shrink" : ""}`}
+        className={`background-video ${isVideoFinished ? "shrink" : ""}`}
         onEnded={handleVideoEnd}
       >
         <source src="/contact-bg.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className={`content ${videoEnded ? "show-content" : ""}`}>
+      <div className={`content ${isVideoFinished ? "show-content" : ""}`}>
         <h2>{t("contact_me")}</h2>
       </div>
-      {isFormVisible && (
-        <div className={`contact-info ${isFormVisible ? "active" : ""}`}>
+      {isVideoFinished && (
+        <div className={`contact-info active`}>
           <a
             href="https://wa.me/1234567890"
             target="_blank"
@@ -73,7 +58,6 @@ const ContactMe = () => {
             className="contact-icon">
             <SiWhatsapp size={52} />
           </a>
-
           <a
             href="mailto:someone@example.com"
             target="_blank"
@@ -81,7 +65,6 @@ const ContactMe = () => {
             className="contact-icon">
             <FiMail size={52} />
           </a>
-
           <a
             href="https://www.facebook.com"
             target="_blank"
