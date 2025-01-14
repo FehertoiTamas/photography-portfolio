9d93a6fa5e205ce04070c5cdff1ddc7a307c7ab2
Tamas <tamas.fehertoi@gmail.com>
2025-01-14 11:52:09 -0500

    visszaalitas

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  reactStrictMode: isProd,
-
-  images: {
-    unoptimized: isProd, // Ha production módban vagy, akkor kikapcsolja az optimalizálást
-  },
-  output: isProd ? 'export' : undefined,
-  basePath: isProd ? '/photography-portfolio' : '',
-  assetPrefix: isProd ? '/photography-portfolio/' : '',
 };
 
 export default nextConfig;
