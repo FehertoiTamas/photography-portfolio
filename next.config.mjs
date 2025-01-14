bd2aa258443fc4ca9c686fef6d320681c1a1cf29
Tamas <tamas.fehertoi@gmail.com>
2025-01-13 19:13:11 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: isProd,
+
   images: {
     unoptimized: isProd, // Ha production módban vagy, akkor kikapcsolja az optimalizálást
   },
